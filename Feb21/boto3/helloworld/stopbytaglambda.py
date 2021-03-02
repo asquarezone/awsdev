8262bc1edd7b7feed85f1a5ec964efc5e05766c1
qtdevops <qtdevops@gmail.com>
2021-03-02 09:42:44 +0530

    Added code to modify system states

--- Feb21/boto3/helloworld/stopbytaglambda.py
+++ Feb21/boto3/helloworld/stopbytaglambda.py
@@ -6,14 +6,29 @@ print('Loading function')
 
 
 def lambda_handler(event, context):
+    # enhance the code to set default values
+    # default env_value = Dev
     env_value = event['tagname']
-    env_name = event['tagvalue'] 
+    # default env_name = env
+    env_name = event['tagvalue']
+    # default action = stop
+    action = event["action"] 
     ec2_resource = boto3.resource('ec2')
     instance_ids = []
     for instance in ec2_resource.instances.all():
         for name_pair in instance.tags:
             if env_name in name_pair.values() and env_value in name_pair.values():
                 instance_ids.append(instance.id)
-                instance.stop()
+                if action == "stop":
+                    instance.stop()
+                elif action == "start":
+                    instance.start()
+                elif action == "delete":
+                    instance.terminate()
+                elif action == "reboot":
+                    instance.reboot()
+                else:
+                    instance.reload()
+
     return instance_ids
     #raise Exception('Something went wrong')
